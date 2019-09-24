8f5d26b74bcf9dcdb23f856c419c14b6383273e4
Brandon Dong <gasingdong@gmail.com>
2019-09-23 20:39:20 -0700

    add apidoc.json

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,13 +1,15 @@
 define({
-  "name": "Back-End",
-  "version": "1.0.0",
-  "description": "",
+  "name": "Trip Split API",
+  "version": "0.1.0",
+  "description": "Documentation for the Trip Split API",
+  "title": "Trip Split API",
+  "url": "https://bd-trip-split.herokuapp.com/api/docs",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-09-24T03:26:55.728Z",
+    "time": "2019-09-24T03:39:05.368Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
