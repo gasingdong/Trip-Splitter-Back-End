18bd717f8ea3bc90c492c230c6d137c666e6e6e1
Brandon Dong <gasingdong@gmail.com>
2019-09-24 09:24:05 -0700

    add docs directory

---
+++ docs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Trip Split API",
+  "version": "0.1.0",
+  "description": "Documentation for the Trip Split API",
+  "title": "Trip Split API",
+  "url": "https://bd-trip-split.herokuapp.com/api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-09-24T16:16:54.677Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
